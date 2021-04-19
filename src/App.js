050d5b7550a6489173222b6ef2591b69b2b0d84f
Amar <gosaviamar681999@gmail.com>
2021-04-19 16:34:10 +0530

    Quiz API added to the App component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import Register from './components/Register';
 function App() {
   const initialUserState = {username:'', password:'', isLoggedin: false};
   const [user, setUser] = useState(initialUserState);
+  const [ques, setQuestion] = useState('');
+
+  const fetchData = async (e) => {
+    const questions = await fetch('https://quizapi.io/api/v1/questions?apiKey=cfN6rMdbSfXXfjE8xsbdTrxNkTUKLoot7zvIsyT2')
+    const data = await questions.json();
+    setQuestion(data[0].question)
+  }
 
   return (
     <Router>
@@ -23,6 +30,13 @@ function App() {
         <Route path='/login' exact user={user} component={Login} />
         <Route path='/register' exact component={Register} />
         
+      </div>
+
+      <div>
+        <button onClick={fetchData}>Fetch Data</button>
+        <p>
+          {ques.length > 0 ? ques: ''}
+        </p>
       </div>
     </Router>
   );
